e2ebf6f0bfae6aa510f49270d0aa2c894d72680e
lochlenyash <79761795+lochlenyash@users.noreply.github.com>
2021-03-02 10:28:13 +0000

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-northeast-1_peRBtkmuT', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '4l24j8cntfs3kqre9icougl0p5', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'ap-northeast-1' // e.g. us-east-2
+        userPoolId: 'eu-west-1_r2RCCniUL', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4eer8ubqlmc6alome49bdlojco', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-west-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: 'https://2n4cb6kz1g.execute-api.ap-northeast-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
